77c22c3d654fb863d67ade0e60e5dcab88ebc0e1
Matheus Henrique <71947546+matheus-551@users.noreply.github.com>
2022-03-21 17:17:59 -0300

    Alteração no sql

--- database/SCHOOL_CONTROL.sql
+++ database/SCHOOL_CONTROL.sql
@@ -19,6 +19,9 @@
 -- Table structure for table `aluno`
 --
 
+CREATE DATABASE `school_control`;
+USE `school_control`;
+
 DROP TABLE IF EXISTS `aluno`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
